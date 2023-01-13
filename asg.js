e28617b4cb72fc59e3ebdf594d91b3c04605ae75
Ritik Rajdev <ritik_rajdev@mckinsey.com>
2023-01-13 15:31:13 +0530

    chore: check if number is prime or armstrong

---
+++ asg.js
@@ -0,0 +1,20 @@
+function isPrime(n) {
+	if (n % 2 === 0)
+		return false;
+	for (let i = 3; i < Math.sqrt(n); i+=2)
+		if (n % i === 0)
+			return false;
+	return true;	
+}
+
+
+function isArmstrong(n) {
+	n = String(n)
+	let sum = 0
+	for (const x of n)
+		sum += Math.pow(Number(x), n.length)
+	return n == sum
+}
+
+console.log(isArmstrong(9474))
+console.log(isPrime(13))
