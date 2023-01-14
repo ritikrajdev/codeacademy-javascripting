03f54190ab2f4dbfbce827aba721b024ebf1d5c6
Ritik Rajdev <ritik_rajdev@mckinsey.com>
2023-01-15 01:17:09 +0530

    feat: add capability to find largest even number

---
+++ assignments/numberArrayUtils.js
@@ -0,0 +1,20 @@
+// Q1
+const isEven = num => num % 2 === 0
+
+function largestEvenNumber(arrayOfNums) {
+	let largestEvenNumber = -1;
+	arrayOfNums.forEach(num => {
+		isEven(num) ? largestEvenNumber = Math.max(largestEvenNumber, num) : null;
+	});
+	return largestEvenNumber;
+}
+
+const largestEvenNumberArrow = (arrayOfNums) => {
+	let largestEvenNumber = -1;
+	arrayOfNums.forEach(num => {
+		isEven(num) ? largestEvenNumber = Math.max(largestEvenNumber, num) : null;
+	});
+	return largestEvenNumber;
+}
+
+
