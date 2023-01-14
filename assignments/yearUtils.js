3a2fdbb8ae1edfcf6c3d17efcb5c5915474e581d
Ritik Rajdev <ritik_rajdev@mckinsey.com>
2023-01-15 02:02:35 +0530

    feat: add capability to check for leap year

---
+++ assignments/yearUtils.js
@@ -0,0 +1,6 @@
+// Q5
+function isLeapYear(year) {
+	return year % 100 == 0 ? year % 400 == 0 : year % 4 == 0;
+}
+
+const isLeapYearArrow = (year) => year % 100 == 0 ? year % 400 == 0 : year % 4 == 0;
